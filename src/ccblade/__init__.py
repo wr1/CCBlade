3a5bdaa45f7184d4abd356de568540c5019a14ca
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-29 15:39:03 +0200

    Update __init__.py imports

--- src/ccblade/__init__.py
+++ src/ccblade/__init__.py
@@ -1,5 +1,6 @@
 """CCBlade: Blade element momentum aerodynamics for wind turbines"""
-from .core.ccblade import CCBlade
-from .airfoil.airfoil import CCAirfoil
-from .airfoil.airfoil import Polar
+
 from .airfoil.airfoil import Airfoil
+from .airfoil.ccairfoil import CCAirfoil
+from .airfoil.polar import Polar
+from .core.ccblade import CCBlade
