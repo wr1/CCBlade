79df7929e50ad82a7b39d27dffcc8101c15722ee
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-29 12:32:08 +0200

    Add admin script for formatting and testing

--- admin.sh
+++ admin.sh
@@ -6,11 +6,6 @@ git commit -m 'Implement BEM functions in Python'
 git add ccblade/ccblade.py
 git commit -m 'Update imports to use Python BEM'
 git add pyproject.toml
-pyproject.toml
-git commit -m 'Update build system to remove Fortran'
-git add setup.py
-git commit -m 'Remove Meson extension from setup'
-git add meson.build
-git commit -m 'Remove Fortran from Meson'
+git commit -m 'Switch to uv and hatch build system'
 git add admin.sh
 git commit -m 'Add admin script for formatting and testing'
